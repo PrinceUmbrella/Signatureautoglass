b6eeb01405654033c01d8d41ddd886e0b48ba6a9
PrinceUmbrella <33269725+PrinceUmbrella@users.noreply.github.com>
2019-04-29 00:20:06 -0400

    Made the title a link to the home page

--- web/freeQuote.php
+++ web/freeQuote.php
@@ -42,7 +42,7 @@
     <nav class="w3-sidebar w3-red w3-collapse w3-top w3-large w3-padding" style="z-index:3;width:300px;font-weight:bold;" id="mySidebar"><br>
         <a href="javascript:void(0)" onclick="w3_close()" class="w3-button w3-hide-large w3-display-topleft" style="width:100%;font-size:22px">Close Menu</a>
         <div class="w3-container">
-            <h3 class="w3-padding-64"><b>Signature<br>Auto Glass</b></h3>
+        <a href="index.php" onclick="w3_close()" class="w3-bar-item w3-button w3-hover-none"><h3 class="w3-padding-64"><b>Signature<br>Auto Glass</b></h3></a>
         </div>
         <div class="w3-bar-block">
             <a href="index.php" onclick="w3_close()" class="w3-bar-item w3-button w3-hover-white">Home</a>
